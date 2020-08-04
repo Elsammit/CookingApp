9ab9f2d58976c75443940132f8da528c4c10aa97
Elsammit <hhashimo25@gmail.com>
2020-08-04 20:52:28 +0900

    Realse Rom commit

---
+++ precache-manifest.140d2cb26e3ffb2cfeeeede60a698cf6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509de70ff25ebb424d67ef221120d513",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9104691fd52997c8a267",
+    "url": "/static/css/main.d12eba88.chunk.css"
+  },
+  {
+    "revision": "1b1103ca3e075c8736ab",
+    "url": "/static/js/2.e79d8b73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e79d8b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9104691fd52997c8a267",
+    "url": "/static/js/main.baec0903.chunk.js"
+  },
+  {
+    "revision": "59f3d9d75ac4526f29f7",
+    "url": "/static/js/runtime-main.d70d6d88.js"
+  },
+  {
+    "revision": "c046b0de5405c6711e1144810c2c3aff",
+    "url": "/static/media/katsukare.c046b0de.jpg"
+  },
+  {
+    "revision": "9b7b1d2523e27713d0bec68f331f86e9",
+    "url": "/static/media/mabodoufu.9b7b1d25.jpg"
+  },
+  {
+    "revision": "b91bf738177e454c1fe2d257f62d0ca9",
+    "url": "/static/media/naporitan.b91bf738.jpg"
+  },
+  {
+    "revision": "0317e081aa95eb8e71087de6f8475ed3",
+    "url": "/static/media/nikuzyaga.0317e081.jpg"
+  }
+]);
